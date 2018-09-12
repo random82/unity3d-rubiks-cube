e67c87b29734b8b46a8bdb4a2b97480ca2e210e2
Dominik Raniszewski <dominik.raniszewski@gmail.com>
2018-09-12 23:20:26 +1000

    Fixed the blocks alingment but rotation doesn't work yet

--- Assets/Scripts/rubik.cs
+++ Assets/Scripts/rubik.cs
@@ -10,16 +10,18 @@ public class rubik : MonoBehaviour {
 
     private IList<GameObject> blocks = new List<GameObject>();
 
+    private bool rotating = false;
+
  	// Use this for initialization
 	IEnumerator Start () {
         for (var i = 0; i < 27; i++)
         {
             yield return null;
-            var instance = Instantiate<GameObject>(CubeBlock);
+            var instance = Instantiate<GameObject>(CubeBlock, transform);
             instance.name = "Block" + i;
-            var x = ((i % 3) / 3.0f) - .3f;
-            var y = (((i / 3) % 3) / 3.0f) - .3f;
-            var z = (((i / 9) % 3) / 3.0f) -.3f;
+            var x = ((i % 3) / 3.0f) - 1/3.0f;
+            var y = (((i / 3) % 3) / 3.0f) - 1/3.0f;
+            var z = (((i / 9) % 3) / 3.0f) - 1/3.0f;
             instance.transform.localPosition = new Vector3(x, y, z);
             blocks.Add(instance);
         }
@@ -40,8 +42,12 @@ public class rubik : MonoBehaviour {
 
     private IEnumerator RotateLeftDown()
     {
+        if (rotating) { yield break; }
+
+        rotating = true;
+
         var rotatingPlane = GameObject.Find("yz1");
-        var rotatingBlocks = blocks.Where(b => b.transform.position.x == -0.3f).ToList();
+        var rotatingBlocks = blocks.Where(b => b.transform.position.x == -1/3f).ToList();
 
         rotatingBlocks.ForEach(b => b.transform.parent = rotatingPlane.transform);
 
@@ -51,22 +57,41 @@ public class rubik : MonoBehaviour {
             yield return null;
         }
 
-        blocks.Where(b => b.transform.position.x == -0.3f).ToList().ForEach(b => b.transform.parent = null);
+        blocks.Where(b => b.transform.position.x == -0.3f).ToList().ForEach(b => b.transform.parent = transform);
+
+        rotating = false;
     }
 
     private IEnumerator RotateLeftUp()
     {
+        if (rotating) { yield break; }
+
+        rotating = true;
+
         var rotatingPlane = GameObject.Find("yz1");
-        var rotatingBlocks = blocks.Where(b => b.transform.position.x == -0.3f).ToList();
+        var rotatingBlocks = blocks.Where(b => b.transform.position.x == -1/3f).ToList();
 
         rotatingBlocks.ForEach(b => b.transform.parent = rotatingPlane.transform);
 
-        for (var i = 0; i <= 60; i++)
+        var currentRot = rotatingPlane.transform.eulerAngles;
+        var targetRot = rotatingPlane.transform.eulerAngles;
+        targetRot.x = Mathf.MoveTowardsAngle(targetRot.x, targetRot.x + 90f, 90f);
+
+        while (Mathf.Abs(currentRot.x - targetRot.x) > 1)
         {
-            rotatingPlane.transform.Rotate(90f * Time.deltaTime, 0, 0);
+            rotatingPlane.transform.Rotate(90f * Time.deltaTime,0,0);
+            currentRot = rotatingPlane.transform.eulerAngles;
+            Debug.LogFormat("Target {0}, current {1}", targetRot.x, rotatingPlane.transform.rotation.eulerAngles);
             yield return null;
         }
 
-        rotatingBlocks.ForEach(b => b.transform.parent = null);
+        if(rotatingPlane.transform.rotation.x != targetRot.x)
+        {
+            rotatingPlane.transform.eulerAngles = targetRot;
+        }
+
+        rotatingBlocks.ForEach(b => b.transform.parent = transform);
+
+        rotating = false;
     }
 }
